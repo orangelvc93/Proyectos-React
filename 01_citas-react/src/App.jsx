625c52ec445c4c534e83b55326adc6e3d9308ee3
Orangel Valdespino <orangelvc93@gmail.com>
2024-01-12 15:00:56 -0500

    Control de citas 0.1

---
+++ 01_citas-react/src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header";
+
+function App() {
+	return (
+		<>
+			<div>
+				<h1></h1>
+			</div>
+		</>
+	);
+}
+
+export default App;
---
+++ 01_citas-react/src/components/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+    
+
+    return(
+        <h1>Desde header</h1>
+    )
+}
+
+export default Header;
